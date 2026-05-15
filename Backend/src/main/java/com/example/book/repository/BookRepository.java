048e42653194e78d44e718e17a58dae2a254de03
yunyuuma <gikiin67@gmail.com>
2026-05-15 11:07:29 +0900

    Backend構築

---
+++ Backend/src/main/java/com/example/book/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.example.book.repository;
+
+import com.example.book.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, String> {
+}
